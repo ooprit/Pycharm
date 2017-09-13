bc50eb96d395b98984178d350b37b8d08929bec1
Olga Opritova <ooprit@yahoo.com>
2017-09-12 23:20:14 -0400

    animal

--- ptut101_animal.py
+++ ptut101_animal.py
@@ -1,7 +1,7 @@
 class Animal:
     def __init__(self, birthType="Unknown",
                  appearance="Unknown", blooded="Unknown"):
-        self.birth = birthType
+        self.birthType = birthType
         self.appearance = appearance
         self.blooded = blooded
     @property
@@ -23,27 +23,65 @@ class Animal:
     def blooded(self, blooded):
         self.__blooded = blooded
     def __str__(self):
-        return "A {} is {} it is {} it is {}".format(type(self).__name__, self.birthType, self.appearance, self.blooded)
-    class Mammal(Animal):
-        def __init__(self, birthType="born alive",
-                     appearance="hair or fur",
-                     blooded="warm blooded",
-                     nurseYoung="True"):
-            Animal.__init__(self, birthType, appearance, blooded)
-            self.nurseYoung = nurseYoung
-        @property
-        def nurseYoung(self):
-            return self.__nurseYoung
-        @nurseYoung.setter
-        def nurseYoung(self, nurseYoung):
-            self.__nurseYoung = nurseYoung
-        def __str__(self):
-            return  super().__str__() + "  and it is {} they nurse their young".format (self.nurseYoung)
+        return "A {} is {} it has {} it is {}".format(type(self).__name__, self.birthType, self.appearance, self.blooded)
+class Mammal(Animal):
+    def __init__(self, birthType="born alive",
+                 appearance="hair or fur",
+                 blooded="warm blooded",
+                 nurseYoung="True"):
+        Animal.__init__(self, birthType, appearance, blooded)
+        self.__nurseYoung = nurseYoung
+    @property
+    def nurseYoung(self):
+        return self.__nurseYoung
+    @nurseYoung.setter
+    def nurseYoung(self, nurseYoung):
+        self.__nurseYoung = nurseYoung
+    def __str__(self):
+        return  super().__str__() + "  and it is {} they nurse their young".format (self.nurseYoung)
 class Reptile (Animal):
     def __init__(self, birthType="born in a egg or born alive",
                  appearance="dry scales",
-                 blooded="cold blood")
+                 blooded="cold blood"):
         Animal.__init__(self, birthType, appearance, blooded)
+
+    def sumAll(self, *args):
+        sum = 0
+        for i in args:
+            sum += i
+        return sum
+
+def getBirthType(theObject):
+    print("the {} is {}".format(type(theObject).__name__,
+                                theObject.birthType))
+
+
+def main():
+    animal1 = Animal("born alive")
+    print (animal1.birthType)
+    print(animal1)
+    print()
+    mammal1 = Mammal()
+    print(mammal1.birthType)
+    print(mammal1.appearance)
+    print(mammal1.blooded)
+    print(mammal1.nurseYoung)
+    print(mammal1)
+
+    reptile1 = Reptile()
+    print(reptile1.birthType)
+    print(reptile1)
+
+    print("summ : {}".format(reptile1.sumAll(1, 2, 3, 4, 5)))
+    getBirthType(mammal1)
+    getBirthType(reptile1)
+
+
+
+main()
+
+
+
 
 
 
